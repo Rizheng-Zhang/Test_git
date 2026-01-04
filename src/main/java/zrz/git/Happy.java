714438af3467ba405c873db6f8b95baccf298289
zrz_eclipse <1002107768_eclipse@qq.com>
2026-01-04 19:36:06 +0800

    edit the branch hot_fix

--- src/main/java/zrz/git/Happy.java
+++ src/main/java/zrz/git/Happy.java
@@ -3,7 +3,6 @@ package zrz.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("left ...");
-		System.out.println("right ...");
 		System.out.println("hot_fix ...");
 	}
 }
