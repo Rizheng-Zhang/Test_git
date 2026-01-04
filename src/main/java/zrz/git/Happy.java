bc9017678c29cc0ad06d4f15c08c041e36305e29
zrz_eclipse <1002107768_eclipse@qq.com>
2026-01-04 19:30:27 +0800

    edit by hot_fix

--- src/main/java/zrz/git/Happy.java
+++ src/main/java/zrz/git/Happy.java
@@ -3,5 +3,7 @@ package zrz.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("left ...");
+		System.out.println("right ...");
+		System.out.println("hot_fix ...");
 	}
 }
